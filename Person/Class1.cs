8e99eb5e1e9f973410019ccac23cb27416e3d29f
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2024-11-23 18:09:17 -0500

    Add project files.

---
+++ Person/Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Persons {
+    /// <summary>
+    /// A class defining a person.
+    /// </summary>
+    public class Person {
+        string name;
+        int age;
+
+        /// <summary>
+        /// Creates a new person with the given name and age.
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="age">The age</param>
+        public Person(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        /// <summary>
+        /// Creates a new person with no name and age.
+        /// </summary>
+        public Person() {
+            this.name = null;
+            this.age = 0;
+        }
+
+        /// <summary>
+        /// Creates a new person with the given name and no age.
+        /// </summary>
+        /// <param name="name">The name of the person.</param>
+        public Person(string name) {
+            this.name = name;
+            this.age = 0;
+        }
+
+        /// <summary>
+        /// Sets the name of the person
+        /// </summary>
+        /// <param name="name">The name to set for the person</param>
+        public void setName(string name) {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Gets the name of the person
+        /// </summary>
+        /// <returns>The person's name</returns>
+        public string getName() {
+            return name;
+        }
+        /// <summary>
+        /// Sets the age of the person
+        /// </summary>
+        /// <param name="age">The person's age</param>
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        /// <summary>
+        /// Gets the age of the person
+        /// </summary>
+        /// <returns>The person's age.</returns>
+        public int getAge() {
+            return age;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the person.
+        /// </summary>
+        /// <returns>A string representing the person.</returns>
+        public override string ToString() {
+            return String.Format("Person Name: {0}, Age: {1}", name, age);
+        }
+    }
+}
---
+++ Person/README.md
@@ -0,0 +1,3 @@
+# README for Person
+
+This is the README for the Person class. The Person class is a class that represents a person. It has a name and an age.
